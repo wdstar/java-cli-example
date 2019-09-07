a482e9b56f6e907417167a959392f4c2ecd1f9be
wdstar <wdstar@users.noreply.github.com>
2019-09-07 16:38:44 +0900

    fix default cry.

--- src/main/java/com/github/wdstar/animals/Animal.java
+++ src/main/java/com/github/wdstar/animals/Animal.java
@@ -5,14 +5,25 @@ package com.github.wdstar.animals;
  */
 public interface Animal {
 
-	public static final String DEFAULT_CRY = "default cry";
+	// @formatter:off
+	public static final String DEFAULT_CRY = "...";
+	public static final String DEFAULT_SLEEP = "zzz";
+	// @formatter:on
 
 	public static String getInfo() {
 		return String.format("Animal: default cry: %s", DEFAULT_CRY);
 	}
 
+	public default String cryAndSleep() {
+		return cry() + ", " + sleep();
+	}
+
 	public default String cry() {
 		return DEFAULT_CRY;
+	}
+
+	public default String sleep() {
+		return DEFAULT_SLEEP;
 	}
 
 }
--- src/test/java/com/github/wdstar/animals/AnimalTest.java
+++ src/test/java/com/github/wdstar/animals/AnimalTest.java
@@ -12,10 +12,12 @@ public class AnimalTest {
 	@Test
 	public void testCry() {
 		Animal thing = new Thing();
-		assertThat(thing.cry()).isEqualTo("default cry");
+		assertThat(thing.cry()).isEqualTo("...");
+		assertThat(thing.cryAndSleep()).isEqualTo("..., zzz");
 
 		Animal cat = new Cat();
 		assertThat(cat.cry()).isEqualTo("mew");
+		assertThat(cat.cryAndSleep()).isEqualTo("mew, zzz");
 	}
 
 }
